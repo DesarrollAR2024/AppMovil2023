8050dd7aec7a9396bee34ef7618d0409cadf0c0d
Deborat Ayala <deboratayala@gmail.com>
2023-09-29 16:52:45 -0300

    navegacion

--- TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/ayuda.java
+++ TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/ayuda.java
@@ -1,13 +1,26 @@
 package com.desarrollar.triviagamer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ayuda extends AppCompatActivity {
-
+ Button btnvolver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+
+        btnvolver= findViewById(R.id.btnvolverhome);
+        btnvolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ayuda.this , Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
