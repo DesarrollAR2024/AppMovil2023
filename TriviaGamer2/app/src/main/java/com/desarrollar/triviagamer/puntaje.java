a6f68f19bd97bf45184a5e3fab75e4d4f426ea96
Triana Devia <trianadevia@gmail.com>
2023-09-29 22:55:14 -0300

    Soluciono conflicto en activity puntaje

--- TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/puntaje.java
+++ TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/puntaje.java
@@ -5,5 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class puntaje extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntaje);
+    }
 }
+
