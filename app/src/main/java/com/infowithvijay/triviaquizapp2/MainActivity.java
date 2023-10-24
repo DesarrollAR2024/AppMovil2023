75802bbce4fd5d1dcbb99766d157f9e42ffe6ab0
matiasp23 <matiaspedraza023@gmail.com>
2023-10-23 23:38:27 -0300

    cambios nuevo registro y login

--- app/src/main/java/com/infowithvijay/triviaquizapp2/MainActivity.java
+++ app/src/main/java/com/infowithvijay/triviaquizapp2/MainActivity.java
@@ -1,41 +1,67 @@
 package com.infowithvijay.triviaquizapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btniniciarsesion;
-    Button btnregistro;
-
-    // declare una variable indicando el tipo de control
-
+    EditText username, password, repassword;
+    Button signup, signin;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btniniciarsesion = findViewById(R.id.butn_iniciarsesionm);
-        btnregistro = findViewById(R.id.butn_iniciarsesionm2);
-        btniniciarsesion.setOnClickListener(new View.OnClickListener() {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        repassword = (EditText) findViewById(R.id.repassword);
+        signup = (Button) findViewById(R.id.btnsignup);
+        signin = (Button) findViewById(R.id.btnsignin);
+        DB = new DBHelper(this);
+
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this , SignUpActivity.class);
-                startActivity(intent);
-            }
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(checkuser==false){
+                            Boolean insert = DB.insertData(user, pass);
+                            if(insert==true){
+                                Toast.makeText(MainActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(MainActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                    }
+                } }
         });
-        btnregistro.setOnClickListener(new View.OnClickListener() {
+
+        signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this , SignUpActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
     }
-
-    //codigo para ir a otra pagina indicando a donde quiero ir!//
 }
