62717a078cd76dfc54ab923a302b050da9f07995
matiasp23 <matiaspedraza023@gmail.com>
2023-10-27 21:27:46 -0300

    cambios en ranking

--- TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/Ranking.java
+++ TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/Ranking.java
@@ -1,8 +1,12 @@
 package com.desarrollar.triviagamer;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,9 @@ import java.util.ArrayList;
 public class Ranking extends AppCompatActivity {
     RecyclerView rankingLista;
 
+    ImageView volverfl;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +35,15 @@ public class Ranking extends AppCompatActivity {
         RankingListAdapter adapter = new RankingListAdapter(rankingList);
 
         rankingLista.setAdapter(adapter);
+
+        volverfl = findViewById(R.id.volverfl);
+        volverfl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Ranking.this, CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
